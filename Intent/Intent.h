2b0009700cd44bab6289d4cf1bf8736473078e03
王杰 <wangjie7@yhd.com>
2017-11-15 15:20:49 +0800

    no message

---
+++ Intent/Intent.h
@@ -0,0 +1,19 @@
+//
+//  Intent.h
+//  Intent
+//
+//  Created by Jerry on 2017/11/15.
+//  Copyright © 2017年 Jerry Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Intent.
+FOUNDATION_EXPORT double IntentVersionNumber;
+
+//! Project version string for Intent.
+FOUNDATION_EXPORT const unsigned char IntentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Intent/PublicHeader.h>
+
+
